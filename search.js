546df1d8bede231ff82236804f96dfa65f8940bc
Ranastra <karo.schnitter@gmx.de>
2023-02-08 22:28:44 +0100

    init

---
+++ search.js
@@ -0,0 +1,6 @@
+var s = "https://www.roblox.com/discover/?Keyword=";
+//console.log("test0");
+function search_roblox(search_query) {
+  //console.log("test1");
+  window.open(s.concat(search_query), "_blank");
+}
